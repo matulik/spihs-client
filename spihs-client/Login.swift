ec92ac686e55e645842efc6be87396a811e38d19
matulik <matulik@o2.pl>
2015-08-06 10:42:58 -0700

    Create Request class

--- spihs-client/Login.swift
+++ spihs-client/Login.swift
@@ -12,9 +12,10 @@ import SwiftyJSON
 
 let host = "http://127.0.0.1:8000/"
 let fjson = "?format=json"
+// Must be implementet of all Request-base classes
 let numberOfCallbacks = 2
 
-class Login {
+class Login : Request {
     var id : Int = -1
     var username : String = ""
     var password : String = ""
@@ -23,9 +24,16 @@ class Login {
     var token : String = ""
     var notificationDictonary = ["Bool": false]
     
-    init() {
-        //        TODO INITIALIZATION
+    override init() {
+        // TODO INITIALIZATION
     }
+    
+    override func callback(id: Int, status: Int, data: AnyObject) {
+        if id == 1 {
+            self.loginCallback(status, data: data)
+        }
+    }
+    
     // ID = 1
     func login() {
         let parameters = [
@@ -59,102 +67,6 @@ class Login {
     // ID = 2
     func logout() {
         self.sendRequest("GET", subpage: "logout/", returnType: "DEFAULT", params: ["":""], callbackID: 2, loging: true)
-    }
-    
-    // parametrers:
-    // methos: GET, POST
-    // returnType: JSON, DEFAULT
-    func sendRequest(method : String, subpage : String, returnType: String, params : [String:String], callbackID: Int, loging: Bool) {
-        // Validate method
-        var method = method.uppercaseString
-        if (method != "GET" && method != "POST") {
-            println("Bad method value")
-            return
-        }
-        // Validete returnType
-        var returnType = returnType.uppercaseString
-        if (returnType != "JSON" && returnType != "DEFAULT") {
-            println("Bad returnType value")
-            return
-        }
-        
-        if (callbackID < 0 || callbackID > numberOfCallbacks) {
-            println("Bad callback value")
-            return
-        }
-        
-        var httpMethod : Alamofire.Method?
-        if method == "GET" {
-            httpMethod = Alamofire.Method.GET
-        }
-        else if method == "POST" {
-            httpMethod = Alamofire.Method.POST
-        }
-        else {
-            httpMethod = nil
-        }
-        
-        switch(returnType) {
-        case "DEFAULT":
-            Alamofire.request(httpMethod!, host+subpage, parameters: params)
-                .response { request, response, data ,error in
-                    var statusCode : Int = 0
-                    if loging == true {
-                        println("### SENDING ###")
-                        println("➡️Request: \(method) on \(subpage) with \(params)\n")
-                        if let resp = response {
-                            statusCode = resp.statusCode
-                            println("⬅️statusCode: \(statusCode)")
-                            println("🔤Response: \(data)")
-                        }
-                        else {
-                            statusCode = 0
-                            println("⛔️\(error)")
-                        }
-                    }
-                    if let ldata: AnyObject = data {
-                        self.callback(callbackID, status: statusCode, data: ldata)
-                    }
-                    else {
-                        self.callback(callbackID, status: statusCode, data: 0)
-                    }
-            }
-            
-        case "JSON":
-            Alamofire.request(httpMethod!, host+subpage+fjson, parameters: params)
-                .responseJSON { request, response, JSON, error in
-                    var statusCode : Int = 0
-                    if loging == true {
-                        println("### SENDING ###")
-                        println("➡️Request: \(method) on \(subpage) with \(params)")
-                        if let resp = response {
-                            statusCode = resp.statusCode
-                            println("⬅️statusCode: \(statusCode)")
-                            println("🔤Response: \(JSON)")
-                        }
-                        else {
-                            statusCode = 0
-                            println("⛔️\(error)")
-                        }
-                    }
-                    if let json: AnyObject = JSON {
-                        self.callback(callbackID, status: statusCode, data: json)
-                    }
-                    else {
-                        self.callback(callbackID, status: statusCode, data: 0)
-                    }
-                    
-            }
-            
-        default:
-            println("default")
-        }
-    }
-    
-    func callback(id: Int, status: Int, data: AnyObject) {
-        if id == 1 {
-            self.loginCallback(status, data: data)
-        }
     }
 }
 
---
+++ spihs-client/Request.swift
@@ -0,0 +1,105 @@
+//
+//  Request.swift
+//  spihs-client
+//
+//  Created by matulik on 06/08/15.
+//  Copyright (c) 2015 mt. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Request {
+    // parametrers:
+    // methos: GET, POST
+    // returnType: JSON, DEFAULT
+    func sendRequest(method : String, subpage : String, returnType: String, params : [String:String], callbackID: Int, loging: Bool) {
+        // Validate method
+        var method = method.uppercaseString
+        if (method != "GET" && method != "POST") {
+            println("Bad method value")
+            return
+        }
+        // Validete returnType
+        var returnType = returnType.uppercaseString
+        if (returnType != "JSON" && returnType != "DEFAULT") {
+            println("Bad returnType value")
+            return
+        }
+        
+        if (callbackID < 0 || callbackID > numberOfCallbacks) {
+            println("Bad callback value")
+            return
+        }
+        
+        var httpMethod : Alamofire.Method?
+        if method == "GET" {
+            httpMethod = Alamofire.Method.GET
+        }
+        else if method == "POST" {
+            httpMethod = Alamofire.Method.POST
+        }
+        else {
+            httpMethod = nil
+        }
+        
+        switch(returnType) {
+        case "DEFAULT":
+            Alamofire.request(httpMethod!, host+subpage, parameters: params)
+                .response { request, response, data ,error in
+                    var statusCode : Int = 0
+                    if loging == true {
+                        println("### SENDING ###")
+                        println("➡️Request: \(method) on \(subpage) with \(params)\n")
+                        if let resp = response {
+                            statusCode = resp.statusCode
+                            println("⬅️statusCode: \(statusCode)")
+                            println("🔤Response: \(data)")
+                        }
+                        else {
+                            statusCode = 0
+                            println("⛔️\(error)")
+                        }
+                    }
+                    if let ldata: AnyObject = data {
+                        self.callback(callbackID, status: statusCode, data: ldata)
+                    }
+                    else {
+                        self.callback(callbackID, status: statusCode, data: 0)
+                    }
+            }
+            
+        case "JSON":
+            Alamofire.request(httpMethod!, host+subpage+fjson, parameters: params)
+                .responseJSON { request, response, JSON, error in
+                    var statusCode : Int = 0
+                    if loging == true {
+                        println("### SENDING ###")
+                        println("➡️Request: \(method) on \(subpage) with \(params)")
+                        if let resp = response {
+                            statusCode = resp.statusCode
+                            println("⬅️statusCode: \(statusCode)")
+                            println("🔤Response: \(JSON)")
+                        }
+                        else {
+                            statusCode = 0
+                            println("⛔️\(error)")
+                        }
+                    }
+                    if let json: AnyObject = JSON {
+                        self.callback(callbackID, status: statusCode, data: json)
+                    }
+                    else {
+                        self.callback(callbackID, status: statusCode, data: 0)
+                    }
+            }
+            
+        default:
+            println("default")
+        }
+    }
+    
+    func callback(id: Int, status: Int, data: AnyObject) {
+        fatalError("Method must be override!")
+    }
+}
